51234fa1189637f6b37f9a59f9d721d0f4e4da2c
esauOp <monkeycoss@gmail.com>
2015-08-07 18:43:30 -0500

    Socket.io for sensor simulation

--- server/rover.js
+++ server/rover.js
@@ -69,3 +69,31 @@ function piMove(pin, state){
 
     x=0;
 }
+
+/****** SERIAL + SOCKET *******/
+
+var io = require('socket.io')();
+io.on('connection', function(socket){});
+io.listen(3000);
+
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort;
+// Instanciamos el objeto de conexión al puerto serial
+var sp = new SerialPort("/dev/ttyACM0", {
+    baudrate: 9600,
+    parser: serialport.parsers.readline("\n")
+});
+
+// Abrimos la conexión al puerto serial
+sp.open(function () {
+
+// Cada que llegue un dato serial lo imprimiremos en la consola
+// En este caso convertimos el dato en Entero y lo mostramos como
+// temperatura en grados celsius.
+
+    sp.on("data", function (data) {
+    var temp = parseInt(data, 10) + " ºC"
+    console.log(temp);
+    io.emit("thermistor",temp);
+    });
+});
